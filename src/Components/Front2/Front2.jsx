7b7848d2c21ea33b5692f8c74136150bb2adbe37
BetterLogic Team <102576102+BetterLogicTeam@users.noreply.github.com>
2022-07-12 00:39:28 -0700

    Last commit

--- src/Components/Front2/Front2.jsx
+++ src/Components/Front2/Front2.jsx
@@ -8,9 +8,9 @@ export default function Front2() {
         <div class="container">
           <div class="row benefits">
             <div class="col-12 text-center">
-              <h1 class="heading-title">BENEFITS OF WHE CLUB NFTs</h1>
+              <h1 class="heading-title">BENEFITS OF ULE CLUB NFTs</h1>
               <p className="kami">
-              Holders of WHE NFTs will get exclusive membership to WHE club a private members only club in the metaverse
+              Holders of ULE NFTs will get exclusive membership to ULE club a private members only club in the metaverse
               </p>
             </div>
           </div>
@@ -35,8 +35,8 @@ export default function Front2() {
                     know it’s real because you see the artist’s signature on the
                     canvas. Somebody can photocopy the painting, but they don’t
                     own it — you do. Before NFTs, digital assets were like
-                    photocopies: When you buy a WHE Club NFT, the artwork is
-                    yours to keep or use in advertising, whether you plan to
+                    photocopies: ULEn you buy a ULE Club NFT, the artwork is
+                    yours to keep or use in advertising, Whether you plan to
                     sell it or not.
                   </p>
                 </div>
@@ -49,16 +49,16 @@ export default function Front2() {
                   {/* <h4>
                     <a href="#">Explore the Metaverse with us</a>
                   </h4> */}
-                  <h1>EXPLORE THE ULEnft's WITH US</h1>
+                  <h1>EXPLORE THE ULE nft's WITH US</h1>
                   <p>
                     The ULEnft's is an online, three-dimensional universe that
                     combines multiple virtual spaces. It can be compared to a
                     future version of the internet. With metaverse, users will
                     be able to collaborate, meet, play games, and socialize in
-                    these 3D spaces. By joining the WHE Club, you will have
+                    these 3D spaces. By joining the ULE Club, you will have
                     access to every metaverse we enter. Plus, there will be
                     additional perks within our metaverse Private members club
-                    for WHE Club NFT holders.
+                    for ULE Club NFT holders.
                   </p>
                 </div>
               </div>
@@ -74,7 +74,7 @@ export default function Front2() {
                     An ULEnft's token has metadata encoded permanently on the
                     blockchain in a format that is "non fungible", it includes a
                     URI which points to a unique JPEG, a piece of art which is
-                    the visual representation of this token, All WHE Club NFTs
+                    the visual representation of this token, All ULE Club NFTs
                     are available as high-resolution jpegs, which allows you to
                     print a high-quality physical copy or display it on screen.
                   </p>
--- src/Components/Front3/Front3.jsx
+++ src/Components/Front3/Front3.jsx
@@ -14,9 +14,16 @@ export default function Front3() {
             </div>
             <div className="col-md-6">
               <div className="head " >
-                <h1>WHE CLUB– YOUR KEY TO EXPLORING METAVERSE</h1>
+                <h1>ULE CLUB– YOUR KEY TO EXPLORING METAVERSE</h1>
                 <p>
-                We believe that one of the most valuable aspects of any community is user generated content. More importantly user generated content that helps the community foster and grow. WHE Metaverse is an interactive 3D universe based in space, with the freedom to explore different virtual worlds and communities, it is not necessarily a game in and of itself. It can instead best be described as a hub for virtual social interaction and expression, and trading and collecting assets within a digital economy, and our WHE Club is the key to unlocking this world. WHE Club NFTs is a limited collection of unique, programmatically generated NFTs hosted on the Polygon blockchain. Each WHE is unique – just like you and programmatically generated from over 200 possible traits, including clothing, city background, expression and more.
+                We believe that one of the most valuable aspects of any community is user generated content.
+                 More importantly user generated content that helps the community foster and grow. 
+                 ULE Metaverse is an interactive 3D universe based in space, with the freedom to explore different virtual 
+                 worlds and communities, it is not necessarily a game in and of itself. It can instead best be described as a
+                  hub for virtual social interaction and expression, and trading and collecting assets within a digital economy,
+                   and our ULE Club is the key to unlocking this world. ULE Club NFTs is a limited collection of unique,
+                    programmatically generated NFTs hosted on the Polygon blockchain. Each ULE is unique – just like you and 
+                    programmatically generated from over 200 possible traits, including clothing, city background, expression and more.
                 </p>
               </div>
               <a className="btn btn11 btn-box" href="#">
